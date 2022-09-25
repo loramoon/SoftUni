67cf5ba672d59dcd273e7ba34bec16c80b7a1bc1
loramoon <112502847+loramoon@users.noreply.github.com>
2022-09-25 19:03:50 +0300

    Update and rename Python Fundamentals/Statistics.py to Python Fundamentals/Dictionary/Statistics.py

--- Python Fundamentals/Dictionary/Statistics.py
+++ Python Fundamentals/Dictionary/Statistics.py
@@ -15,13 +15,3 @@ for key in products:
     print(f"- {key}: {products[key]}")
 print(f"Total Products: {len(products.keys())}")
 print(f"Total Quantity: {sum(products.values())}")
-
-
-
-
-
-
-
-
-
-
