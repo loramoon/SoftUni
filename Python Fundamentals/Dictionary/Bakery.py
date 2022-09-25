78626b36a4a64e1b6961e2970cb455206da17f50
loramoon <112502847+loramoon@users.noreply.github.com>
2022-09-25 19:08:20 +0300

    Add files via upload

---
+++ Python Fundamentals/Dictionary/Bakery.py
@@ -0,0 +1,23 @@
+elements = input().split(' ')
+my_dict = {}
+
+for i in range (0, len(elements), 2):
+    key = elements[i]
+    value = elements[i+1]
+    my_dict[key] = int(value)
+
+print(my_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
