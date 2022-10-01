b6208e6d1a33e322b7510e2ef1545bd39dcacf1e
loramoon <112502847+loramoon@users.noreply.github.com>
2022-10-02 00:34:42 +0300

    Add files via upload

---
+++ Python Advanced/Error Handling/email_validator.py
@@ -0,0 +1,28 @@
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+email = input()
+domains = ['com', 'bg', 'net', 'org']
+
+while email != "End":
+    if '@' not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+    name = email.split('@')
+    if len(name[0]) <= 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+    domain = name[1].split('.')
+    if domain[1] not in domains:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+
+    print("Email is valid")
+
+    email = input()
