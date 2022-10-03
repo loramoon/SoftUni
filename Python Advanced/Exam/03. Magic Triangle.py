45528fc19e9d26bf40121c53f677e45aa21ba03b
loramoon <112502847+loramoon@users.noreply.github.com>
2022-10-03 10:00:51 +0300

    Add files via upload

---
+++ Python Advanced/Exam/03. Magic Triangle.py
@@ -0,0 +1,13 @@
+def get_magic_triangle(n):
+    result = []
+    for cur_row_idx in range(n):
+        result.append([1])
+        for internal_idx in range(1, cur_row_idx):
+            internal_sum = result[cur_row_idx - 1][internal_idx - 1] + result[cur_row_idx - 1][internal_idx]
+            result[cur_row_idx].append(internal_sum)
+        if cur_row_idx != 0:
+            result[cur_row_idx].append(1)
+    return result
+
+
+print(get_magic_triangle(5))
