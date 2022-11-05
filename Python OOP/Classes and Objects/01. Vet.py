9d74a4955a5596e84a515f12ff7516e2d454f882
loramoon <112502847+loramoon@users.noreply.github.com>
2022-11-05 19:23:51 +0200

    Update 01. Vet.py

--- Python OOP/Classes and Objects/01. Vet.py
+++ Python OOP/Classes and Objects/01. Vet.py
@@ -7,16 +7,16 @@ class Vet:
         self.animals = []
 
     def register_animal(self, animal_name):
-        if len(Vet.animals) < Vet.space:
+        if len(self.animals) < Vet.space:
             self.animals.append(animal_name)
             Vet.animals.append(animal_name)
             return f"{animal_name} registered in the clinic"
         return "Not enough space"
 
     def unregister_animal(self, animal_name):
-        if animal_name in Vet.animals and animal_name in self.animals:
-            Vet.animals.remove(animal_name)
+        if animal_name in self.animals:
             self.animals.remove(animal_name)
+            Vet.animals.remove(animal_name)
             return f"{animal_name} unregistered successfully"
         return f"{animal_name} not in the clinic"
 
