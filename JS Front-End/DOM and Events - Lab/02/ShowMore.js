f2caf42222c726a337048b24a672e63708174c43
loramoon <112502847+loramoon@users.noreply.github.com>
2023-04-17 18:10:58 +0300

    Add files via upload

---
+++ JS Front-End/DOM and Events - Lab/02/ShowMore.js
@@ -0,0 +1,11 @@
+function showText() {
+    const link = document.getElementById('more');
+    const text = document.getElementById('text');
+    
+    // show the hidden text
+    text.style.display = 'inline';
+  
+    // hide the link
+    link.style.display = 'none';
+  }
+  
