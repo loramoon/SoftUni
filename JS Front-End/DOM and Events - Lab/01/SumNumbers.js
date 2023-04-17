99588f7a8dde3fcf023fd627498d8b7cf0369602
loramoon <112502847+loramoon@users.noreply.github.com>
2023-04-17 18:09:52 +0300

    Add files via upload

---
+++ JS Front-End/DOM and Events - Lab/01/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let sum = num1 + num2;
+    document.getElementById('sum').value = sum;
+}
